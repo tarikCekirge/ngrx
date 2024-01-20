e4bbfa00d154b34bd5b0eb9a90f4a52b0f11e21f
Tarik Cekirge <tcekirge@gmail.com>
2024-01-20 20:37:47 +0300

    Counter Reducer & Store Setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { counterReducer } from '../app/store/counter.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore()]
+  providers: [provideRouter(routes), provideStore({ counter: counterReducer })]
 };
---
+++ src/app/store/counter.reducer.ts
@@ -0,0 +1,14 @@
+import { createReducer } from "@ngrx/store";
+
+const initialState = 0;
+
+// export const counterReducer = createReducer(
+//     initialState)
+
+export function counterReducer(state = initialState) {
+    return state
+}
+
+
+
+
